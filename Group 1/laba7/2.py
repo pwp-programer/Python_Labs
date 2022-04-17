061a733efa9e48c96273ee1b80b8b982416a8b5a
pwp-programer <bogdanzadora2005@gmail.com>
2022-04-17 14:56:13 +0300

    adead ex2 in laba7

--- Group 1/laba7/2.py
+++ Group 1/laba7/2.py
@@ -1,27 +1,38 @@
 # Создание первого множества
 x1 = []
-for i in range(1, 21):
+for i in range(100, 201):
     x1.append(i)
 
 # Создание второго множества
 x2 = []
-for i in range(10, 31):
+for i in range(100, 201):
     x2.append(i)
 
 # Создание третьего множества
 x3 = []
-for i in range(1, 22, 2):
+for i in range(100, 201):
     x3.append(i)
+
 
 # Преобразование в множество
 x1 = set(x1)
 x2 = set(x2)
 x3 = set(x3)
 
-print(x1, x2, x3)
+# print(x1, x2, x3)
 
 
 # Создание мегамножества
-y = (x1 | x2) * (x1 | x3) / (x2 + x3)
-y = set(y)
+print(x1 + "\n")
+y = set((x1 & x2) | (x1 | x3))
 print(y)
+
+if x1 in y:
+    print("x1 in y = true")
+else:
+    print("x1 in y = false")
+
+if 150 in y:
+    print("150 in y = true")
+else:
+    print("150 in y = false")
